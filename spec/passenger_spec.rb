92527a3571b6579555c61860a8da8473e5e15584
Tom Coakes <tomcoakes@gmail.com>
2015-02-09 17:48:35 +0000

    Created files: peoplecontainer and peoplecontainer_spec

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -6,26 +6,4 @@ describe Passenger do
   let(:coach) {double :coach}
   let(:station) {double :station}
 
-  it "can board a train" do
-    expect(coach).to receive(:push)
-    expect(station).to receive(:delete)
-    passenger.board_train(coach, station)
-  end
-
-  it "can alight from a coach" do
-    expect(coach).to receive(:delete)
-    expect(station).to receive(:push)
-    passenger.alight(coach, station)
-  end
-
-  it "can enter a station" do
-    expect(station).to receive(:push)
-    passenger.enter_station(station)
-  end
-
-  it "can leave a station" do
-    expect(station).to receive(:delete)
-    passenger.leave_station(station)
-  end
-
 end
